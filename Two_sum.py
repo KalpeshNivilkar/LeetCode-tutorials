1c02f58fd4821b08363f72f590f17d58e2d3fb0c
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-07-03 11:35:33 +0530

    add new code

--- Two_sum.py
+++ Two_sum.py
@@ -1,12 +1,24 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
 
-# Create object and call method
-sol = Solution()
-arr = [2, 7, 11, 15]
+# # Create object and call method
+# sol = Solution()
+# arr = [2, 7, 11, 15]
+# target = 9
+# print(sol.twoSum(arr, target))  # Output: [0, 1]
+
+
+
+def twoSum(nums,target):
+    for i in range (len(nums)):
+        for j in range(i + 1 ,len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+
+arr = [2,7,12,15]
 target = 9
-print(sol.twoSum(arr, target))  # Output: [0, 1]
+print(twoSum(arr,target))
